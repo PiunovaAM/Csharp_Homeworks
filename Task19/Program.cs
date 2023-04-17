e1606ddad5a9dde0fe39c8b839199e9a5967d536
Настя <Piu@192.168.1.7>
2023-04-17 18:37:37 +0300

    Add task 19

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -1,39 +1,44 @@
-﻿//double d = Math.Sqrt(5); 
-//double d1 = 5.09987654;
-//double d1Round = Math.Round(d1, 2, MidpointRounding.ToZero);
-//Console.WriteLine(d1Round);
-//double d = Math.Round(Math.Sqrt(5), 2);
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
 
-//Задача 20: Напишите программу, которая
-//принимает на вход координаты двух точек и
-//находит расстояние между ними в 2D
-//пространстве.
-//A (3,6); B (2,1) -> 5,09
-//A (7,-5); B (1,-1) -> 7,21
-
-
-double DistanceAB(int xa, int ya, int xb, int yb)
+void Palindrome (string number)
 {
-    int deltaX = xa - xb;
-    int deltaY = ya - yb;
-    double distAB = Math.Sqrt(deltaX * deltaX + deltaY * deltaY);   
-    return distAB;
+    if (number [0] == number [4] && number [1] == number [3])
+    {
+        Console.Write($"Your {number} is palindrome");
+    }
+    else Console.Write($"Your {number} isn't palindrome");
 }
 
-Console.WriteLine("Введите координаты точки A  ");
-Console.Write("Xa: ");
-int xACoordinate = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input your five-digit number: ");
+string? number = Console.ReadLine();
 
-Console.Write("Ya: ");
-int yACoordinate = Convert.ToInt32(Console.ReadLine());
+if (number!.Length == 5)
+{
+    Palindrome(number);
+}
+else Console.WriteLine($"Input right number");
 
-Console.WriteLine("Введите координаты точки B  ");
-Console.Write("Xb: ");
-int xBCoordinate = Convert.ToInt32(Console.ReadLine());
+// или
+/*
+void Palindrome (int number)
+{
+    if(number%10 != number/10%10 & number%10 != number/100%10 & number/10%10 != number/100%10)
+    {
+        Console.Write($"{number} is palindrome");
+    }
+    else Console.Write($"{number} is NOT palindrome");
+}
+Console.Write("Input your five-digit number: ");
+int user_number = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Yb: ");
-int yBCoordinate = Convert.ToInt32(Console.ReadLine());
-
-double distanceAB = DistanceAB(xACoordinate, yACoordinate, xBCoordinate, yBCoordinate);
-double distRound = Math.Round (distanceAB, 2, MidpointRounding.ToZero);
-Console.WriteLine(distRound);
+if (user_number > 10000 & user_number < 100000)
+{
+    Palindrome(user_number);
+}
+else Console.WriteLine("Input five-digit number");
+*/
