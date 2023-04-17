a286184ecf4e3c469b1b35d46e99b79e4b116889
Настя <Piu@192.168.1.7>
2023-04-17 15:35:31 +0300

    Add Task 22

---
+++ Task22/Program.cs
@@ -0,0 +1,13 @@
+﻿void TableSquare(int num)
+{
+    int count = 1;
+    while (count <= num )
+    {
+        Console.WriteLine($"{count}  {count * count}");
+        count+=1;
+    }
+}
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+TableSquare(number);
