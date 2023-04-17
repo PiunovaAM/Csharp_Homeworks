b8f2fa3627d1ac48fb3f949daf61df0438ced23b
Настя <Piu@192.168.1.7>
2023-04-17 18:38:01 +0300

    Add task 23

---
+++ Task23/Program.cs
@@ -0,0 +1,25 @@
+﻿//Задача 23
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+void Cubes(int uN)
+{
+    int current = 1; // ввели перменную, которую будем возводить в куб
+    while (current <= uN) //ограничили условием, что переменная не должна превышать user number (uN)
+    {
+        int res = current * current * current; // ввели доп.переменную, которая считает куб
+        Console.Write($"{res}, ");
+        current++; // повторяем цикл до завершения (current +1)
+    }
+    Console.WriteLine("\b\b.");
+}
+Console.Write("Input number: ");
+int user_number = Convert.ToInt32(Console.ReadLine());
+
+if (user_number >= 1)
+{
+    Console.Write($"{user_number} -> ");
+    Cubes(user_number);
+}
+else Console.Write("Impossible value!");
