2ab90e46057a8c31fe4e1d09de4d55a7d787c03a
Настя <Piu@192.168.1.7>
2023-04-18 18:54:10 +0300

    Add Task 29

---
+++ Task29/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 29: Напишите программу, которая
+// 1. задаёт массив из 8 элементов и 
+// 2. выводит их на экран.
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+
+Console.Write("Введите длину массива: ");
+int lenghtArray = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = CreateArray(lenghtArray);
+PrintArray(arr);
+
+int[] CreateArray(int size)
+{
+    int[] array = new int [size];
+    Random random = new Random(); // оптимизация объекта Random (var - объект неявной типизации)
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(0, 100);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write($"{array[array.Length -1]}");
+    Console.WriteLine("]");
+}
