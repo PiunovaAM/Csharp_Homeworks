ca157744da499f8d48ac9404a35fc820b27820ca
Настя <Piu@192.168.1.7>
2023-04-18 18:43:19 +0300

    Change to NET 6.0

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -15,7 +15,7 @@ else
     Console.WriteLine("Your number isn't 3-digit!");
 }
 */
-
+/*
 Console.Write("Input your number: ");
 int anyNumber = Convert.ToInt32(Console.ReadLine());
 string anyNumberText = Convert.ToString(anyNumber);
@@ -28,3 +28,21 @@ else
 {
   Console.WriteLine("Your number isn't 3-digit!");
 }
+*/
+
+int ThirdDigit (int num)
+{
+    int count = num;
+    while (num > 999)
+    {
+        num = num / 10;
+        count++;
+    }
+        return num % 10;
+}
+
+Console.WriteLine("Введите любое число больше 99: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string res = (number >= 100) ? $"третья цифра: {ThirdDigit(number)}" : "третьей цифры нет";
+Console.WriteLine(res);
