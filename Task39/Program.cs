af6fd3898fc6e81c6ae2f3d272033eeec81cc76d
Настя <Piu@192.168.1.7>
2023-04-28 11:13:16 +0300

    Add task 39

---
+++ Task39/Program.cs
@@ -0,0 +1,25 @@
+﻿//39. Перевернуть массив
+//  0 1 2 3 4
+// [1 2 3 4 5] -> [5 4 3 2 1]
+//Array.Reverse(array);
+
+void ReverseArray (int[] arr)
+{
+    for (int i = 0; i < arr.Length / 2; i++)
+    {
+        int temp = arr[i];
+        arr[i] = arr[arr.Length - 1 - i];
+        arr[arr.Length - 1 - i] = temp;
+    }
+}
+
+int[] array = CreateArrayRndInt(6, 1, 25);
+PrintArray(array);
+
+Console.WriteLine();
+ReverseArray(array);
+PrintArray(array);
+
+Console.WriteLine();
+Array.Reverse(array);
+PrintArray(array);
