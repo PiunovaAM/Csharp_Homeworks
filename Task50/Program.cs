14ec1ebcdcefc779a5ab0eaf28d2c2f624844a26
Настя <Piu@192.168.1.7>
2023-05-05 18:32:04 +0300

    Add Task 50

---
+++ Task50/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1, 7 -> такого элемента в массиве нет
+
+Console.Write("Введите номер строки: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите номер столбца: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int [,] numbers = new int [5,5];
+
+FillMatrixRandomNumbers(numbers);
+PrintMatrix(numbers);
+
+void FillMatrixRandomNumbers(int[,] matrix)
+{
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) 
+        {
+            matrix[i, j] = rnd.Next(-10, 10);
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine(" |");
+    }
+}
+
+
+if (n > numbers.GetLength(0) || m > numbers.GetLength(1))
+{
+    Console.WriteLine($"{n},{m} -> такого элемента нет");
+}
+else
+{
+    Console.WriteLine($"{n},{m} -> Ваш элемент {numbers[n-1,m-1]}");
+}
+
